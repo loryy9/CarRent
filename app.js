c4fce053baded7a387f2936e53b83090b8d0f4d2
Lorenzo Bellotti <bellotti.lorenzo14@gmail.com>
2025-05-09 19:33:53 +0200

    Ordinato app.js

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const express = require("express")
 const session = require("express-session")
 const passport = require("passport")
 const morgan = require("morgan")
+const db = require("./models/db")
 
 const PORT = 3000
 const app = express()
 
-// Connessione al database
-const db = require("./models/db")
+const indexRouter = require("./routes/index")
+const registrazioneRouter = require("./routes/registrazione")
+const loginRouter = require("./routes/login")
+const logoutRouter = require("./routes/logout")
+const dashboardRouter = require("./routes/dashboard")
+const catalogoRouter = require("./routes/catalogo")
 
-// Definizione del motore ejs
 app.set("view engine", "ejs")
 
-// Definizione MIDDLEWARE
 app.use(morgan("tiny"))
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Definizione della sessione passport
 app.use(session({
     secret: "CarRent",
     resave: false,
@@ -28,16 +30,6 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-// Importazione router
-const indexRouter = require("./routes/index")
-const registrazioneRouter = require("./routes/registrazione")
-const loginRouter = require("./routes/login")
-const logoutRouter = require("./routes/logout")
-const dashboardRouter = require("./routes/dashboard")
-const catalogoRouter = require("./routes/catalogo")
-
-
-console.log("Router importati con successo")
 app.use(indexRouter)
 app.use(registrazioneRouter)
 app.use(loginRouter)
